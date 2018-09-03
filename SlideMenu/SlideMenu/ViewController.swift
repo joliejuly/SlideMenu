22fd820a29786dfdea93de44272204dcfb502f06
Julia Nikitina <julianikitina.ios@gmail.com>
2018-09-03 20:46:44 +0300

    Initial commit

---
+++ SlideMenu/SlideMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideMenu
+//
+//  Created by Julia Nikitina on 03/09/2018.
+//  Copyright © 2018 Julia Nikitina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
